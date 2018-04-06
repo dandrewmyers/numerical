9182e515d03aa565ecf6da30c15e6afc394f474f
David A. Myers <damyers1973@gmail.com>
2018-04-05 22:11:56 -0400

    updates to mc_pi and new mc_pi_arry with faster array calculation

--- mc_pi.py
+++ mc_pi.py
@@ -2,30 +2,33 @@
 #Outputs pi estimate in terminal and also plot based on number of random points inputed by user.
 #This code based on previous version at https://gist.github.com/louismullie/3769218.
 
-import random as r
 import numpy as np
 import matplotlib.pyplot as plt
+import time as time
 
 # input total number of random points
 total_random_points = int(input("\nNumber of random points for Monte Carlo estimate of Pi?\n>"))
+
+# start time of calculation
+start_time = time.time()
 
 # number of random points inside unit cicrle and total random points
 inside_circle = 0
 
 # create empty x and y arrays for eventual scatter plot of generated random points
-x_plot_array = np.empty((0,total_random_points))
-y_plot_array = np.empty((0,total_random_points))
+x_plot_array = np.empty(shape=(1,total_random_points))
+y_plot_array = np.empty(shape=(1,total_random_points))
 
 # generate random points and count points inside unit circle
 # top right quadrant of unit cicrle only
 for i in range(0, total_random_points):
     # print(f'\nIteration: {i + 1}')
     # generate random x, y in range [0, 1]
-    x = r.random()
+    x = np.random.rand()
     x_plot_array = np.append(x_plot_array, [x])
     #print(f'{x_plot_array}')
     # print(f'x value: {x}')
-    y = r.random()
+    y = np.random.rand()
     y_plot_array = np.append(y_plot_array, [y])
     #print(f'{y_plot_array}')
     # print(f'y value: {y}')
@@ -38,14 +41,15 @@ for i in range(0, total_random_points):
     # print(f'Points inside circle {inside_circle}')
     # print(f'Number of random points {i+1}')
     # calc approximate pi value
-    pi_approx = (float(inside_circle) / (i+1)) * 4
+    pi_approx = inside_circle / (i+1) * 4
     # print(f'Approximate value for pi: {pi_approx}')
 
 # final numeric output for pi estimate
 print ("\n--------------")
 print (f"\nApproximate value for pi: {pi_approx}")
 print (f"Difference to exact value of pi: {pi_approx-np.pi}")
-print (f"Percent Error: (approx-exact)/exact * 100: {(pi_approx-np.pi)/np.pi*100} %\n")
+print (f"Percent Error: (approx-exact)/exact*100: {(pi_approx-np.pi)/np.pi*100}%")
+print (f"Execution Time: {time.time() - start_time} seconds\n")
 
 # plot output of random points and circle, top right quadrant only
 random_points_plot = plt.scatter(x_plot_array, y_plot_array, color='blue', s=.1)
